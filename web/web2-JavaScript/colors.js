3e1697f695460e7dffc874f080e26421b3595ad5
hermin9804 <hermin9804@gmail.com>
2022-01-19 01:33:29 +0900

    [FEAT] jquery

--- web/web2-JavaScript/colors.js
+++ web/web2-JavaScript/colors.js
@@ -1,20 +1,22 @@
-
 let Links = {
     setColor:function linkSetColor(color) {
-        let alist = document.querySelectorAll('a');
-        let i = 0;
-        while (i < alist.length) {
-            alist[i].style.color = color;
-            i++;
-        }
+        // let alist = document.querySelectorAll('a');
+        // let i = 0;
+        // while (i < alist.length) {
+        //     alist[i].style.color = color;
+        //     i++;
+        // }
+        $('a').css('color', color);
     }
 }
 let Body = {
     setColor:function (color){
-        document.querySelector('body').style.color = color;
+        // document.querySelector('body').style.color = color;
+        $('body').css('color', color);
     },
     setBackgroundColor:function (color){
-        document.querySelector('body').style.backgroundColor = color;
+        // document.querySelector('body').style.backgroundColor = color;
+        $('body').css('backgroundColor', color);
     }
 }
 function nightDayHandler(self) {
