964eb1153ffc77ba174739f21ac0d14053abe8da
hermin9804 <hermin9804@gmail.com>
2022-01-20 15:49:48 +0900

    [FEAT] add MainNavigation

--- web/react/use-router/src/App.js
+++ web/react/use-router/src/App.js
@@ -4,14 +4,18 @@ import { Routes } from "react-router-dom";
 import AllMeetupPage from "./pages/AllMeetups";
 import NewMeetupPage from "./pages/NewMeetup";
 import FavoritsPage from "./pages/Favorites";
+import MainNavigation from "./components/layout/MainNavigation";
 
 function App() {
   return (
-    <Routes>
-      <Route exact path="/" element={<AllMeetupPage />} />
-      <Route exact path="/new-meetup" element={<NewMeetupPage />} />
-      <Route exact path="/favorites" element={<FavoritsPage />} />
-    </Routes>
+    <div>
+      <MainNavigation />
+      <Routes>
+        <Route exact path="/" element={<AllMeetupPage />} />
+        <Route exact path="/new-meetup" element={<NewMeetupPage />} />
+        <Route exact path="/favorites" element={<FavoritsPage />} />
+      </Routes>
+    </div>
   );
 }
 
