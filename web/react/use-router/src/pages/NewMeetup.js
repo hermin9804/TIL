1759a9d82975d68d08b0b6d97c1eb24440f948fc
hermin9804 <hermin9804@gmail.com>
2022-01-21 16:31:12 +0900

    [FEAT] useEffect with firebase

--- web/react/use-router/src/pages/NewMeetup.js
+++ web/react/use-router/src/pages/NewMeetup.js
@@ -1,10 +1,28 @@
+import { useNavigate } from "react-router-dom";
+
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
+  const navigate = useNavigate();
+
+  function addMeetupHandler(meetupData) {
+    fetch(
+      "https://react-getting-started-77865-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-Type": "appliction/json",
+        },
+      }
+    ).then(() => {
+      navigate("/");
+    });
+  }
   return (
     <section>
       <h1>New Meetup Page</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   );
 }
