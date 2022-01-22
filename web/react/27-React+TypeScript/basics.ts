cfe1702c557ff183946ee6214493dbac3f1a4d32
hermin9804 <hermin9804@gmail.com>
2022-01-22 18:12:32 +0900

    [FEAT] basics typescript

--- web/react/27-React+TypeScript/basics.ts
+++ web/react/27-React+TypeScript/basics.ts
@@ -10,17 +10,36 @@ isIstructor = true;
 let hobbies: string[];
 hobbies = ["Sports", "Cooking"];
 
-let person: {
+type Person = {
   name: string;
   age: number;
 };
 
-person = {
-  name: "max",
-  age: 32,
-};
+let people: Person[];
 
-let people: {
-  name: string;
-  age: number;
-}[];
+// Type inference
+let course: string | number = "React - the Complete Guide";
+
+course = 123;
+
+// Function & Type
+function myAdd(a: number, b: number): number | string {
+  return a + b;
+}
+
+function myPrint(vlaue: any) {
+  console.log(vlaue);
+}
+
+// Generics
+function insetAtBeginning<T>(array: T[], value: T) {
+  const newArray = [value, ...array];
+  return newArray;
+}
+
+const demoArray = [1, 2, 3];
+
+const updatedArray = insetAtBeginning(demoArray, -1); // [-1, 1, 2, 3]
+const stringArray = insetAtBeginning(["a", "b", "c"], "d");
+
+// error ubdatedArray[0].split('');
