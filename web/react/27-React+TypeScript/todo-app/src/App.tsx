d0a216a2a74d929c98200976931fc6f57a281ae2
hermin9804 <hermin9804@gmail.com>
2022-01-25 19:39:59 +0900

    [FEAT] add addTodoHandler

--- web/react/27-React+TypeScript/todo-app/src/App.tsx
+++ web/react/27-React+TypeScript/todo-app/src/App.tsx
@@ -1,13 +1,22 @@
-import "./App.css";
-import Todo from "./components/todo";
+import NewTodo from "./components/NewTodo";
+import Todo from "./models/todo";
 import Todos from "./components/Todos";
-
-const todos = [new Todo("Learn React"), new Todo("Learn TypeScript")];
+import { useState } from "react";
 
 function App() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const addTodoHandler = (todotext: string) => {
+    const newTodo = new Todo(todotext);
+
+    setTodos((prevTodos) => {
+      return prevTodos.concat(newTodo);
+    });
+  };
+
   return (
     <div>
-      <h1>Todo List</h1>
+      <NewTodo onAddTodo={addTodoHandler} />
       <Todos items={todos} />
     </div>
   );
