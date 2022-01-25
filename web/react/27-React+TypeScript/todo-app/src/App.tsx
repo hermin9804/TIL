0d4bfe7267ede8a8939912853d70520b8e0f78a3
hermin9804 <hermin9804@gmail.com>
2022-01-25 17:32:08 +0900

    [TEST]

--- web/react/27-React+TypeScript/todo-app/src/App.tsx
+++ web/react/27-React+TypeScript/todo-app/src/App.tsx
@@ -1,7 +1,16 @@
 import "./App.css";
+import Todo from "./components/todo";
+import Todos from "./components/Todos";
+
+const todos = [new Todo("Learn React"), new Todo("Learn TypeScript")];
 
 function App() {
-  return <div>hi</div>;
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <Todos items={todos} />
+    </div>
+  );
 }
 
 export default App;
