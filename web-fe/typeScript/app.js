1e2bab8902d59c0340e55655dcf3f62faa4b1379
hermin9804 <hermin9804@gmail.com>
2022-02-14 00:45:47 +0900

    [feat] arr

--- web-fe/typeScript/app.js
+++ web-fe/typeScript/app.js
@@ -1,5 +1,12 @@
 var person = {
     name: "mher",
-    age: 25
+    age: 25,
+    hobbies: ["sports", "Cooking"]
 };
+var favoriteActivities;
+favoriteActivities = ["Sports"];
 console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby);
+}
--- web-fe/typeScript/app.ts
+++ web-fe/typeScript/app.ts
@@ -1,6 +1,14 @@
 const person = {
   name: "mher",
   age: 25,
+  hobbies: ["sports", "Cooking"],
 };
 
+let favoriteActivities: string[];
+favoriteActivities = ["Sports"];
+
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby);
+}
