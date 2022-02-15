cebe9c9ba087e8452aa0de2f38d6fe6138473a05
hermin9804 <hermin9804@gmail.com>
2022-02-15 17:17:56 +0900

    [feat] enum,

--- web-fe/typeScript/app.js
+++ web-fe/typeScript/app.js
@@ -1,12 +1,25 @@
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
 var person = {
     name: "mher",
     age: 25,
-    hobbies: ["sports", "Cooking"]
+    hobbies: ["sports", "Cooking"],
+    role: Role.ADMIN
 };
+//person.role.push("admin");
+//person.role[1] = 10;
+person.role;
 var favoriteActivities;
 favoriteActivities = ["Sports"];
 console.log(person.name);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby);
+}
+if (person.role === Role.ADMIN) {
+    console.log("is admin");
 }
--- web-fe/typeScript/app.ts
+++ web-fe/typeScript/app.ts
@@ -1,8 +1,20 @@
+enum Role {
+  ADMIN,
+  READ_ONLY,
+  AUTHOR,
+}
+
 const person = {
   name: "mher",
   age: 25,
   hobbies: ["sports", "Cooking"],
+  role: Role.ADMIN,
 };
+
+//person.role.push("admin");
+//person.role[1] = 10;
+
+person.role;
 
 let favoriteActivities: string[];
 favoriteActivities = ["Sports"];
@@ -11,4 +23,8 @@ console.log(person.name);
 
 for (const hobby of person.hobbies) {
   console.log(hobby);
+}
+
+if (person.role === Role.ADMIN) {
+  console.log("is admin");
 }
