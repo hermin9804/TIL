d1434f281c3f67ad0ecea0f64d650b43ee88b6a5
hermin9804 <hermin9804@gmail.com>
2022-02-19 17:24:03 +0900

    62. "private" and "public" Access Modifiers

--- web-fe/typeScript/src/app.js
+++ web-fe/typeScript/src/app.js
@@ -1,13 +1,24 @@
 var Department = /** @class */ (function () {
     function Department(n) {
+        this.employees = [];
         this.name = n;
     }
     Department.prototype.describe = function () {
         console.log("Department: " + this.name);
     };
+    Department.prototype.addEmployee = function (employee) {
+        this.employees.push(employee);
+    };
+    Department.prototype.printEmployeeInformation = function () {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    };
     return Department;
 }());
 var accounting = new Department("Accounting");
+accounting.addEmployee("Max");
+accounting.addEmployee("Menu");
 accounting.describe();
-var accountingCopy = { name: "dummy", describe: accounting.describe };
-accountingCopy.describe();
+accounting.printEmployeeInformation();
+// const accountingCopy = { name: "dummy", describe: accounting.describe };
+// accountingCopy.describe();
--- web-fe/typeScript/src/app.ts
+++ web-fe/typeScript/src/app.ts
@@ -1,5 +1,6 @@
 class Department {
-  name: string;
+  public name: string;
+  private employees: string[] = [];
 
   constructor(n: string) {
     this.name = n;
@@ -8,12 +9,27 @@ class Department {
   describe(this: Department) {
     console.log("Department: " + this.name);
   }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
 }
 
 const accounting = new Department("Accounting");
 
+accounting.addEmployee("Max");
+accounting.addEmployee("Menu");
+
+//accounting.employees[2] = "anna";
+
 accounting.describe();
+accounting.printEmployeeInformation();
 
-const accountingCopy = { name: "dummy", describe: accounting.describe };
+// const accountingCopy = { name: "dummy", describe: accounting.describe };
 
-accountingCopy.describe();
+// accountingCopy.describe();
